4aa824c9a1d5b11429032a47850f71557d51540e
gitchaofang <fangchao118@gmail.com>
2026-07-23 20:52:13 -0500

    update

--- pyTorch_fundamentals.py
+++ pyTorch_fundamentals.py
@@ -72,4 +72,98 @@ tensor_ele_mul = tensor * tensor # * = torch.mul() for element-wise multiplicati
 print(f"tensor element-wise multiplication: {tensor_ele_mul}")
 tensor_mat_mul = torch.matmul(tensor,tensor) #tensor @ tensor also works for matrix multiplication
 print(f"tensor matrix multiplication: {tensor_mat_mul}")
+random_tensor1 = torch.rand(size=(3,4)
+                              ,dtype = torch.float16,
+                              requires_grad=True)
+random_tensor2 = torch.rand(size=(4,5)
+                              ,dtype = torch.float16,
+                              requires_grad=False)
+result_tensor = torch.matmul(random_tensor1,random_tensor2)
+print(f"result tensor: {result_tensor}")
+print(f"result tensor shape: {result_tensor.shape}")
+print(f"result tensor dimension: {result_tensor.ndim}")
+print(f"result tensor datatype: {result_tensor.dtype}")
 
+# transpose
+ori_tensor = torch.rand(size = (3,4))
+trans_tensor = ori_tensor.T
+result_tensor = torch.matmul(ori_tensor,trans_tensor)
+print(f"original tensor shape: {ori_tensor.shape}")
+print(f"transposed tensor shape: {trans_tensor.shape}")
+print(f"result tensor shape: {result_tensor.shape}")
+
+# tensor aggregation
+x = torch.arange(1,100,10)
+y = torch.rand(size = (3,4))
+# find mean
+mean_x = torch.mean(x.type(torch.float32))
+mean_y = torch.mean(y, axis = 0)
+print(f"mean of x: {mean_x}")
+print(f"mean of y: {mean_y}")
+# find sum
+sum_x = torch.sum(x)
+sum_y = torch.sum(y, axis = 1)
+print(f"sum of x: {sum_x}") 
+print(f"sum of y: {sum_y}")
+# find max
+max_x = torch.max(x)
+max_y = torch.max(y, axis = 1)
+print(f"max of x: {max_x}")
+print(f"max of y: {max_y}")
+
+# find positional
+argmax_x = torch.argmax(x)
+argmax_y = torch.argmax(y, axis = 0)
+print(f"argmax of x: {argmax_x}")
+print(f"argmax of y: {argmax_y}")
+print(f"argmax of y type: {argmax_y.shape}")
+
+# Reshapin, stacking, squeezing, unsqueezing tensors
+# reshape
+x = torch.arange(1.,10.)
+x_reshaped = x.reshape(3,3)
+print(f"x reshaped: {x_reshaped}")
+
+# stacking
+x_stacked = torch.stack([x,x,x], axis=0)
+print(f"x stacked: {x_stacked}")
+
+#squeezing
+x_squeeze = torch.squeeze(x_stacked, axis=0)
+print(f"x squeezed: {x_squeeze}")
+
+#unszueezing
+x_unsqueeze = torch.unsqueeze(x_squeeze, axis=0)
+print(f"x unsqueezed shape: {x_unsqueeze.shape}")
+
+# permute
+# permute is a view.
+x_orirginal = torch.rand(size=(224,224,3))
+x_permute = x_orirginal.permute(2,0,1)
+print(f"x original shape: {x_orirginal.shape}")
+print(f"x permuted shape: {x_permute.shape}")
+
+#indexing and selecting data from tensors
+x = torch.arange(1,10).reshape(1,3,3)
+print(f"x: {x}")
+print(f"x[0]: {x[0]}")
+print(f"x[0,0]: {x[0][0]}")
+print(f"x[0,0,0]: {x[0][0][0]}")
+
+# use ":" to select all elements in a dimension
+print(f"x[:,:,1]: {x[:,:,1]}")
+print(f"x[:,1,1]: {x[:,1,1]}")
+
+# Numpy to Torch tensor and vice versa
+# numpy to torch tensor
+array = np.arange(1.0,8.0)
+tensor = torch.from_numpy(array) # defalut dtype is float64
+print(f"numpy array: {array}, type: {type(array)}")
+print(f"torch tensor: {tensor}, type: {type(tensor)}")
+
+# torch tensor to numpy
+tensor = torch.ones(size = (3,4))
+numpy_tensor = tensor.numpy() # default dtype is float32
+print(f"torch tensor: {tensor}, type: {type(tensor)}")
+print(f"numpy tensor: {numpy_tensor}, type: {type(numpy_tensor)}")
+print(f"numpy tensor data type: {numpy_tensor.dtype}")

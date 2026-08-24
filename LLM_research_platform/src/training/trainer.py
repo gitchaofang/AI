b4e649004b7845ac24f6ad18ed648de0b5c5900e
gitchaofang <fangchao118@gmail.com>
2026-08-23 18:15:44 -0700

    trainer.py

---
+++ LLM_research_platform/src/training/trainer.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn.functional as F
+
+class Trainer:
+    def __init__(self, model, optimizer, device):
+        self.model = model
+        self.optimizer = optimizer
+        self.device = device
+
+    def train_stap(self, x: torch.Tensor, y: torch.Tensor):
+        x = x.to(self.device)
+        y = y.to(self.device)
+
+        self.optimizer.zero_grad()
+
+        logits = self.model(x)
+
+        B, T, V = logits.shape
+
+        loss = F.cross_entropy(
+            logits.reshape(B * T, V),
+            y.reshape(B*T),
+        )
+
+        loss.backwards()
+        self.optimzer.step()
+
+        return loss.item()

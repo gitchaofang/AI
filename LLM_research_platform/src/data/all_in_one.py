780f593d0df24bef387304fbd1310fd327a8c76b
gitchaofang <fangchao118@gmail.com>
2026-08-26 21:12:41 -0700

    add all in one

--- LLM_research_platform/src/data/all_in_one.py
+++ LLM_research_platform/src/data/all_in_one.py
@@ -11,7 +11,7 @@ class VariableLengthDataset(Dataset):
     def __getitem__(self,idx):
         sentence = self.sentences[idx]
         return{
-            "input_id": sentence[:-1],
+            "input_ids": sentence[:-1],
             "labels": sentence[1:]
         }
     def get_length(self,idx):
@@ -82,6 +82,51 @@ class TokenBatchSampler(Sampler):
 
         for batch in batches:
             yield batch
+
+    class PaddingCollator:
+        def __init__(self,
+                     token_pad_ids = 0,
+                     label_pad_ids = -100,):
+            self.token_pad_ids = token_pad_ids
+            self.label_pad_ids = label_pad_ids
+
+        def __call__(self, batch):
+            batch_size = len(batch)
+            max_len = max(len(item["input_ids"])
+                          for item in batch)
+
+            input_ids = torch.fill(
+                (batch_size, max_len),
+                self.token_pad_ids,
+                dtype = torch.int64,
+            )
+
+            label_ids = torch.fill(
+                (batch_size, max_len),
+                self.label_pad_ids,
+                dtype = torch.int64,
+            )
+
+            attention_mask = torch.zeros(
+                batch_size,
+                max_len,
+                dtype = torch.int64,
+            )
+
+            for i, item in enumerate(batch):
+                length = len(item["input_ids"])
+                input_ids[i, :length] = item["input_ids"]
+                label_ids[i, :length] = item["labelss"]
+                attention_mask[i, : length] = 1
+
+
+            return {
+                "input_ids": input_ids,
+                "labels": label_ids,
+                "attention_mask": attention_mask,
+            }
+
+
 
 
 

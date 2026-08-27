7ce4af86a455a80b4440bc9dddcfed8bb24b4ec0
gitchaofang <fangchao118@gmail.com>
2026-08-26 21:08:30 -0700

    update

--- LLM_research_platform/src/models/transformer.py
+++ LLM_research_platform/src/models/transformer.py
@@ -3,29 +3,29 @@ import torch.nn as nn
 from .SelfAttention import SelfAttention
 
 class FeedForward(nn.Module):
-    def __init__(self, d_model: int, mlp_ratio: int = 4):
+    def __init__(self, d_model: int, mlp_ratio = 4):
         super().__init__()
-
         hidden = d_model * mlp_ratio
         self.net = nn.Sequential(
-            nn.linear(d_model, hidden),
+             nn.Linear(d_model, hidden),
             nn.GELU(),
-            nn.Linear(hidden, d_model),
+            nn.Linear(hidden, d_model)
         )
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         return self.net(x)
 
 class TransformerBlock(nn.Module):
-    def __init__(self, d_model: int, n_heads: int, max_seq_len: int, mask: torch.Tensor):
+    def __init__(self, d_model: int, n_heads:int, max_seq_len: int):
         super().__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.max_seq_len = max_seq_len
 
-        self.norm1 == nn.LayerNorm(d_model)
-        self.attention = SelfAttention(d_model, n_heads, max_seq_len, mask)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.attention = SelfAttention(d_model, n_heads, max_seq_len)
         self.norm2 = nn.LayerNorm(d_model)
         self.ffn = FeedForward(d_model)
-
-    def forward(self, x: torch.Tensor):
-        x = x + self.attention(self.norm1(x))
+    def forward(self, x: torch.Tensor, mask):
+        x = x + self.attention(self.norm1(x), mask)
         x = x + self.ffn(self.norm2(x))
-
-        return x 
+        return x

56ed9c934ddaa2b968d54edcca1903624b5941b4
gitchaofang <fangchao118@gmail.com>
2026-08-27 15:50:14 -0700

    update

--- LLM_research_platform/src/training/trainer.py
+++ LLM_research_platform/src/training/trainer.py
@@ -6,23 +6,22 @@ class Trainer:
         self.model = model
         self.optimizer = optimizer
         self.device = device
-
-    def train_stap(self, x: torch.Tensor, y: torch.Tensor, mask: torch.Tensor):
+    def train_step(self, x: torch.Tensor, y: torch.Tensor, mask: torch.Tensor):
         x = x.to(self.device)
         y = y.to(self.device)
 
         self.optimizer.zero_grad()
 
-        logits = self.model(x,mask)
+        logits = self.model(x, mask)
 
         B, T, V = logits.shape
-
         loss = F.cross_entropy(
             logits.reshape(B * T, V),
-            y.reshape(B*T),
+            y.reshape(B * T),
+            ignore_index = -100,
         )
 
         loss.backwards()
-        self.optimzer.step()
+        self.optimizer.step()
 
         return loss.item()

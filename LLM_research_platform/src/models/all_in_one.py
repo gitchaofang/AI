4f8e659853b1b96e5de1f4de9f053ef2a11f3ab2
gitchaofang <fangchao118@gmail.com>
2026-08-30 17:22:46 -0700

    update

--- LLM_research_platform/src/models/all_in_one.py
+++ LLM_research_platform/src/models/all_in_one.py
@@ -249,9 +249,9 @@ print("Device:", device)
 model = GPT(
     vocab_size = vocab_size,
     d_model = 128,
-    max_seq_len = 512,
+    max_seq_len = 256,
     n_layers = 4,
-    n_heads = 4,
+    n_heads = 2,
 ).to(device)
 
 optimizer = torch.optim.AdamW(

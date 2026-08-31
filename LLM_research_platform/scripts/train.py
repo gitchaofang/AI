a6acca988b3eb03d4963c7f68cf81356f9f2d6a2
gitchaofang <fangchao118@gmail.com>
2026-08-31 12:12:41 -0700

    update

--- LLM_research_platform/scripts/train.py
+++ LLM_research_platform/scripts/train.py
@@ -71,8 +71,8 @@ print("Device:", device)
 model = GPT(
     vocab_size = vocab_size,
     d_model = 128,
-    max_seq_len = 128,
-    n_layers = 4,
+    max_seq_len = 512,
+    n_layers = 2,
     n_heads = 4,
 ).to(device)
 

b08680c5724035859c71ae47fcd0402e132fa519
gitchaofang <fangchao118@gmail.com>
2026-08-31 11:47:10 -0700

    update

--- LLM_research_platform/src/data/token_batch_sampler.py
+++ LLM_research_platform/src/data/token_batch_sampler.py
@@ -6,7 +6,7 @@ class TokenBatchSampler(Sampler):
     def __init__(self,
                  dataset,
                  max_token,
-                 batch_size = 2,
+                 batch_size = 4,
                  shuffle = True):
         self.dataset = dataset
         self.max_token = max_token

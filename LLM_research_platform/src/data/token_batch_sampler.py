0e1a9a1417e48da00fba41778672dea4a2bf1fc6
gitchaofang <fangchao118@gmail.com>
2026-08-31 11:16:58 -0700

    update

--- LLM_research_platform/src/data/token_batch_sampler.py
+++ LLM_research_platform/src/data/token_batch_sampler.py
@@ -5,14 +5,14 @@ class TokenBatchSampler(Sampler):
     def __init__(self,
                  dataset,
                  max_token,
+                 batch_size = 2,
                  shuffle = True):
         self.dataset = dataset
         self.max_token = max_token
         self.shuffle = shuffle
+        self.batch_size = batch_size
         self.indices = list(range(len(dataset)))
-    def __len__(self):
-        return len(self.dataset)
-
+    
     def __iter__(self):
         indices = sorted(self.indices,
                          key = lambda i:
@@ -24,9 +24,9 @@ class TokenBatchSampler(Sampler):
 
         for idx in indices:
             length = self.dataset.get_length(idx)
-            if(length + current_token > self.max_token):
+            if(length + current_token > self.max_token or len(current_batch) >= self.batch_size):
                 batches.append(current_batch)
-                batches = []
+                current_batche = []
                 current_token = 0
             current_batch.append(idx)
             current_token += length
@@ -39,3 +39,22 @@ class TokenBatchSampler(Sampler):
 
         for batch in batches:
             yield batch
+            
+    def __len__(self):
+        indices = sorted(self.indices,
+                     key = lambda i: self.dataset.get_length(i))
+    
+        num_batches = 0
+        current_token = 0
+    
+        for idx in indices:
+            length = self.dataset.get_length(idx)
+            if length + current_token > self.max_token and current_token > 0:
+                num_batches += 1
+                current_token = 0
+            current_token += length
+    
+        if current_token > 0:
+            num_batches += 1
+    
+        return num_batches

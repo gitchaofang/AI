59ab994efa2ca953d26c43320ce31cf2c36a4a2c
gitchaofang <fangchao118@gmail.com>
2026-09-03 14:46:49 -0700

    update

--- LLM_research_platform/src/data/all_in_one.py
+++ LLM_research_platform/src/data/all_in_one.py
@@ -34,7 +34,6 @@ class VariableLengthDataset(Dataset): # for txt file
             if current_len == 255:
                 out.append(current_batch)          
                 current_len = 0
-                print(f"len(out): lentgh is {len(current_batch)}")
                 current_batch = []
         return out
         

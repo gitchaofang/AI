1f61b72cd8b29691a7cc524ed16f84b0efa4dfec
gitchaofang <fangchao118@gmail.com>
2026-09-03 14:14:17 -0700

    update

--- LLM_research_platform/src/data/tokenizer.py
+++ LLM_research_platform/src/data/tokenizer.py
@@ -1,21 +1,22 @@
 class Tokenizer:
-	def __init__(self,file_namd):
-		path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data" + self.file_name
+	def __init__(self,file_name):
+		path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/" + file_name
 		with open(path, "r", encoding="utf-8") as f:
 			text = f.read()
 		words = set()
 		for word in text.split():
 			words.add(word)
 		word_set = sorted(words)
-		stoi = {
+		self.stoi = {
 			i + 1: word 
 			for i, word in enumerate(words)
 		}
-		itos = {
+		self.itos = {
 			word: i + 1
 			for i, word in enumerate(words)
 		}
-		print(f"vocab_size is: {len(stoi)}")
+		print(f"vocab_size is: {len(self.stoi)}")
+	def get():
 		return {
 			"stoi": self.stoi,
 			"itos": self.itos

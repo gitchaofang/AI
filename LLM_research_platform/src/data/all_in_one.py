fd63110047cf02bfb79f1d680c42bb142d744b9e
gitchaofang <fangchao118@gmail.com>
2026-09-03 15:39:51 -0700

    update

--- LLM_research_platform/src/data/all_in_one.py
+++ LLM_research_platform/src/data/all_in_one.py
@@ -32,7 +32,7 @@ class VariableLengthDataset(Dataset): # for txt file
         for word in text.split():
             current_batch.append(word)
             current_len += 1
-            if current_len == 256:
+            if current_len == 128:
                 out.append(current_batch)          
                 current_len = 0
                 current_batch = []

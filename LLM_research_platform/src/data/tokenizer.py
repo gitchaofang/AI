3afb09f1fcd104b968dc3052f3725a2384e34a61
gitchaofang <fangchao118@gmail.com>
2026-09-03 15:03:31 -0700

    update

--- LLM_research_platform/src/data/tokenizer.py
+++ LLM_research_platform/src/data/tokenizer.py
@@ -1,7 +1,7 @@
 class Tokenizer:
 	def __init__(self,file_name):
 		#path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/" + file_name # for local
-		path = "/content/AI/LLM_research_platform/src/data/input_data/data/" + self.file_name # for codlab online # for codelab online
+		path = "/content/AI/LLM_research_platform/src/data/input_data/data/" + file_name # for codlab online # for codelab online
 		with open(path, "r", encoding="utf-8") as f:
 			text = f.read()
 		words = set()

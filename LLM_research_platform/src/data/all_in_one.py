7de1617c007d582a35f4450a1fa4c8a553753086
gitchaofang <fangchao118@gmail.com>
2026-09-03 17:43:41 -0700

    update

--- LLM_research_platform/src/data/all_in_one.py
+++ LLM_research_platform/src/data/all_in_one.py
@@ -32,7 +32,7 @@ class VariableLengthDataset(Dataset): # for txt file
         for char in text:
             current_batch.append(char)
             current_len += 1
-            if current_len > 150 and (char == '.' or char == '\n'):
+            if current_len > 200 and (char == '.' or char == '\n'):
                 out.append(current_batch)          
                 current_len = 0
                 current_batch = []

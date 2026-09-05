e16adef5698642262faf29e47ddaaf40b197d6c8
gitchaofang <fangchao118@gmail.com>
2026-09-05 12:46:35 -0700

    upate

--- LLM_research_platform/src/data/all_in_one.py
+++ LLM_research_platform/src/data/all_in_one.py
@@ -37,7 +37,8 @@ class VariableLengthDataset(Dataset): # for txt file
         for char in text:
             current_batch.append(char)
             current_len += 1
-            if current_len > 200 and (char == '.' or char == '\n'):
+    #        if current_len > 200 and (char == '.' or char == '\n'):
+            if current_len == 256:
                 out.append(current_batch)          
                 current_len = 0
                 current_batch = []
@@ -110,7 +111,8 @@ class TokenBatchSampler(Sampler):
 
         for idx in indices:
             length = self.dataset.get_length(idx)
-            if(length + current_token > self.max_batch_tokens_cnt or len(current_batch) >= self.batch_size):
+#            if(length + current_token > self.max_batch_tokens_cnt or len(current_batch) >= self.batch_size):
+            if len(current_batch) >= self.batch_size:
                 batches.append(current_batch)
                 current_batch = []
                 current_token = 0

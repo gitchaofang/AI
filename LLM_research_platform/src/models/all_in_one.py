8dcc95db550d63c7b7bc3c469874251c95bbf776
gitchaofang <fangchao118@gmail.com>
2026-09-05 13:20:53 -0700

    upate

--- LLM_research_platform/src/models/all_in_one.py
+++ LLM_research_platform/src/models/all_in_one.py
@@ -170,6 +170,8 @@ class GPT(nn.Module):
         self.lm_linear = nn.Linear(d_model,vocab_size + 1,)
         self.position_offset = None
 
+        self.apply(self._init_weights)
+
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)

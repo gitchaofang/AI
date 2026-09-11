19677a7a1c9952954937d6ef278d83d782c6d4d4
gitchaofang <fangchao118@gmail.com>
2026-09-11 16:06:36 -0700

    update

--- LLM_research_platform/src/data/regex_tokenizer.py
+++ LLM_research_platform/src/data/regex_tokenizer.py
@@ -8,7 +8,7 @@ GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
 
 class RegexTokenizer:
-    def __init__(self,filename, pattern = None, vocab_size = 255):
+    def __init__(self,filename, pattern = None, vocab_size = 50257):
         #path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/" + file_name # for local
         path = "/content/AI/LLM_research_platform/src/data/input_data/data/" + filename # for codlab online # for codelab online
         with open(path, "r", encoding="utf-8") as f:
@@ -45,10 +45,10 @@ class RegexTokenizer:
         self.merge_dict = merge_dict
         self.vocab_dict = vocab_dict
 
-    def _encode_chunk(self, chunk_ids): #chunk is list of decimal representations of bytes from the entire text after applying formate seperation
+    def _encode_chunk(self, chunk_ids): #chunk_ids is list of decimal representations of bytes from the entire text after applying formate seperation
         counts = {}
         while len(chunk_ids) >= 2:
-            counts = get_stats(chunk_ids)
+            get_stats(chunk_ids,counts)
             pair = min(counts, key = lambda p: self.merge_dict.get(p, float("inf")))
             if pair not in self.merge_dict:
                 break
@@ -56,7 +56,7 @@ class RegexTokenizer:
             chunk_ids = merge(chunk_ids, pair, idx)
         return chunk_ids
 
-    def encode(self, text):
+    def encode(self, text): # text is pure python string, return a list of decimal represention of bytes
         chunks_text = re.findall(self.pattern, text)
         chunks_list = [list(ch.encode("utf-8")) for ch in chunks_text] # list of list (decimal representations of bytes)
         encoded_ids = []
@@ -64,7 +64,7 @@ class RegexTokenizer:
             encoded_ids.append(self._encode_chunk(chunk_ids))
         return encoded_ids
 
-    def decode(self, ids): #ids is a list of integers, returrn python string
+    def decode(self, ids): #ids is a list of integers, return a python string
         bytes_list = []
         for idx in ids:
             if idx in self.vocab_dict:

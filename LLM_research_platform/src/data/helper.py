71c6b92ade6302db4666a4de6186f1cd1c50d9d4
gitchaofang <fangchao118@gmail.com>
2026-09-10 19:02:20 -0700

    update

---
+++ LLM_research_platform/src/data/helper.py
@@ -0,0 +1,21 @@
+import regex as re
+
+def get_stats(ids, counts=None): #update counts
+    counts = {} if counts is None else counts
+    for pair in zip(ids[:-1],ids[1:]):
+        counts[pair] = counts[pair] + 1
+    return counts
+
+def merge(ids,pair,idx):
+    newids = []
+    i = 0
+    while i < len(ids):
+        if (ids[i] == pair[0] and i != len[ids] - 1 and ids[i + 1] == pair[1]):
+            newids.append(idx)
+            i += 2
+        else:
+            newids.append(ids[i])
+            i += 1
+    return newids
+        
+

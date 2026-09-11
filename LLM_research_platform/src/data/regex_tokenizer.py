31e245ced1dc495614ebfca0f384150f915aff48
gitchaofang <fangchao118@gmail.com>
2026-09-10 19:06:02 -0700

    update

--- LLM_research_platform/src/data/regex_tokenizer.py
+++ LLM_research_platform/src/data/regex_tokenizer.py
@@ -7,7 +7,7 @@ from .helper import merge
 GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
 
-class GPT2Tokenizer:
+class RegexTokenizer:
     def __init__(self,filename, pattern = None, vocab_size = 255):
         #path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/" + file_name # for local
         path = "/content/AI/LLM_research_platform/src/data/input_data/data/" + filename # for codlab online # for codelab online

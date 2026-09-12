c087886afa2aba6c03afdf76998ce2ca46678f42
gitchaofang <fangchao118@gmail.com>
2026-09-12 16:22:40 -0700

    update

--- LLM_research_platform/src/data/regex_tokenizer.py
+++ LLM_research_platform/src/data/regex_tokenizer.py
@@ -13,9 +13,9 @@ GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1
 # tokenization starts from 2
 TOKEN_OFFSET = 2
 class RegexTokenizer:
-    def __init__(self,filename, pattern = None, vocab_size = 50000):
-        #path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/" + file_name # for local
-        path = "/content/AI/LLM_research_platform/src/data/input_data/data/" + filename # for codlab online # for codelab online
+    def __init__(self, filename, pattern = None, vocab_size = 50000):
+        path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/" + file_name # for local
+        #path = "/content/AI/LLM_research_platform/src/data/input_data/data/" + filename # for codlab online # for codelab online
         with open(path, "r", encoding="utf-8") as f:
             self.text = f.read() 
 

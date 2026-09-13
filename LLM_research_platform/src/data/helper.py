9e039370850315c8ec8dfe1de09c3c50f4013c12
gitchaofang <fangchao118@gmail.com>
2026-09-12 17:12:41 -0700

    update

--- LLM_research_platform/src/data/helper.py
+++ LLM_research_platform/src/data/helper.py
@@ -9,7 +9,7 @@ def merge(ids,pair,idx):
     newids = []
     i = 0
     while i < len(ids):
-        if (ids[i] == pair[0] and i != len[ids] - 1 and ids[i + 1] == pair[1]):
+        if (ids[i] == pair[0] and i != len(ids) - 1 and ids[i + 1] == pair[1]):
             newids.append(idx)
             i += 2
         else:

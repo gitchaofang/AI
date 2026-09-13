ced9904d5bfb9b80de4dcad4b78f6dda762353bc
gitchaofang <fangchao118@gmail.com>
2026-09-12 18:17:52 -0700

    update

--- LLM_research_platform/src/data/helper.py
+++ LLM_research_platform/src/data/helper.py
@@ -13,7 +13,7 @@ def merge(ids,pair,idx):
             newids.append(idx)
             i += 2
         else:
-            newids.append(idx[i])
+            newids.append(ids[i])
             i += 1
     return newids
         

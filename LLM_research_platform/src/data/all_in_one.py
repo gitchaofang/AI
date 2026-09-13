b1753a8038f56e7b4dde63704e58727590992be9
gitchaofang <fangchao118@gmail.com>
2026-09-13 14:55:00 -0700

    update

--- LLM_research_platform/src/data/all_in_one.py
+++ LLM_research_platform/src/data/all_in_one.py
@@ -1,173 +1,128 @@
 import torch
 import random
+import numpy as np
+import os
 from torch.utils.data import Dataset
 from torch.utils.data import Sampler
-from src.data.simple_tokenizer import SimpleTokenizer
-# data dir: /Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data
+from src.data.regex_tokenizer import RegexTokenizer
+
+LOCAL_PATH =  "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data"
+COLAB_PATH = "/content/AI/LLM_research_platform/src/data/input_data/data/"
+#seperation pattern
+GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
+
 
 class VariableLengthDataset(Dataset): # for txt file
-    def __init__(self, file_name):
-        self.file_name = file_name
-        print(f"dataset is: {file_name}")
-        sentences = self._data_prep()
-        tokenizer = SimpleTokenizer("novel.txt")
-        token_map = tokenizer.get()
-        self.stoi = token_map["stoi"]
-        self.itos = token_map["itos"]
+    def __init__(self, tokenizer, max_len, filename):
+        read_path = LOCAL_PATH + filename
+        self.token_path = LOCAL_PATH + "tokens.bin"
+        self.index_path = LOCAL_PATH + "index.npy"
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        with open(read_path, "r", encoding="utf-8") as f:
+                                self.text = f.read()
+        if not (os.path.exists(self.token_path) and os.path.exists(self.index_path)):
+            self.data_prep()
 
-        self.tokens = []
-        cnt = 0
-        for sentence in sentences:
-            cnt += len(sentence)
-            current_sentence = [self.stoi[char] for char in sentence]
-            self.tokens.append(current_sentence) 
-        print(f"sample size is: {cnt}")
-        print(f"token size = {len(self.tokens)}")
-        print(f"token number = {len(self.stoi)}")
-        print("\n")
+        self.tokens = np.memmap(
+             self.token_path,
+             dtype = np.int32,
+             mode="r"
+        )
+        self.index = np.load(
+             self.index_path,
+             mmap_mode = "r"
+        )
 
-    def _data_prep(self): # read data then return a list of lis (a batch of data)
-        #path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/" + self.file_name # for local
-        path = "/content/AI/LLM_research_platform/src/data/input_data/data/" + self.file_name # for codlab online
-        with open(path, "r", encoding="utf-8") as f:
-            text = f.read()
-        out = []
-        current_batch = []
-        current_len = 0
-        for char in text:
-            current_batch.append(char)
-            current_len += 1
-            if current_len > 200 and (char == '.' or char == '\n'):
-    #        if current_len == 256:
-                out.append(current_batch)          
-                current_len = 0
-                current_batch = []
-        return out
-        
+    def data_prep(self):
+        encoded_tokens = self.tokenizer.encode(self.text)
+        all_tokens = [item for token_list in encoded_tokens for item in token_list]
+        all_token_len = len(all_tokens)
+        all_index = []
+        offset = 0
 
+        while offset < all_token_len:
+            end = min(offset + self.max_len, all_token_len)
+            all_index.append([offset, end - offset])
+            offset = end
+
+        # Convert to NumPy
+        all_tokens = np.asarray(all_tokens,dtype=np.int32)
+        index = np.asarray(all_index,dtype=np.int64)
+
+        # Save
+        all_tokens.tofile(self.token_path)
+        np.save(self.index_path, index)
 
     def __len__(self):
-        return len(self.tokens)
-    def __getitem__(self,idx):
-        sentence = torch.tensor(self.tokens[idx])  # Convert to tensor here
-        return {
-            "input_ids": sentence[:-1],
-            "labels": sentence[1:]
-        }
-    def get_vocab_size(self):
-        return len(self.stoi)
-    def get_length(self,idx):
-        return len(self.tokens[idx])
+         return len(self.index)
+    
+    def __getitem__(self, key):
+         offset,length = self.index[key]
+         tokens = self.tokens[offset: offset + length]
+         data = torch.tensor(tokens, dthpe = torch.int64)
+         return {
+              "input_ids": data[:-1],
+              "labels": data[1:]
+         }
 
-class BucketSampler(Sampler):
-    def __init__(self,
-                 dataset,
-                 max_len,
-                 shuffle = True,
-                 ):
-        self.dataset = dataset
-        self.max_len = max_len
-        self.shuffle = shuffle
-
-        self.indices = list(range(len(dataset)))
-
-    def __iter__(self):
-        indices = sorted(self.indices,
-                         key = lambda i:
-                            self.dataset.get_length(i))
-
-        batches = []
-
-        for i in range(0,len(indices),self.max_len):
-            batch = indices[i: i + self.max_len]
-            batches.append(batch)
-
-        if self.shuffle:
-            random.shuffle(batches)
-
-        for batch in batches:
-            yield batch
+    def get_length(self, key):
+         return self.index[key][1]
+        
 
 class TokenBatchSampler(Sampler):
     def __init__(self,
-                 dataset,
-                 max_batch_tokens_cnt,
-                 batch_size = 8,
-                 shuffle = True):
-        self.dataset = dataset
-        self.max_batch_tokens_cnt = max_batch_tokens_cnt
-        self.shuffle = shuffle
+                  batch_size,
+                  shuffle = True):
+        index_path = COLAB_PATH + "index.npy"
+        self.index = np.load(
+                     index_path,
+                     mmap_mode = "r"
+                ) 
         self.batch_size = batch_size
-        self.indices = list(range(len(dataset)))
-    
-    def __iter__(self):
-        indices = sorted(self.indices,
-                         key = lambda i:
-                            self.dataset.get_length(i))
+        self.shuffle = shuffle
+        self.indices = list(range(len(self.index)))
+        self.batches = []
+        self._make_batches()
 
-        batches = []
-        current_batch = []
-        current_token = 0
+    def _make_batches(self):
+        indices = sorted(self.indices, key = lambda i: self.index[i][1])
+        
+        for idx in range(0, len(indices), self.batch_size):
+            batch = indices[idx: idx + self.batch_size]
+            self.batches.append(batch)
 
-        for idx in indices:
-            length = self.dataset.get_length(idx)
-#            if(length + current_token > self.max_batch_tokens_cnt or len(current_batch) >= self.batch_size):
-            if len(current_batch) >= self.batch_size:
-                batches.append(current_batch)
-                current_batch = []
-                current_token = 0
-            current_batch.append(idx)
-            current_token += length
+        if self.shullfle:
+            random.shuffle(self.batches)
 
-        if len(current_batch) > 0:
-            batches.append(current_batch)
+    def __iter__(self): 
+        for batch in self.batches:
+              yield batch
 
-        if self.shuffle:
-            random.shuffle(batches)
-
-        for batch in batches:
-            yield batch
-            
     def __len__(self):
-        indices = sorted(self.indices,
-                     key = lambda i: self.dataset.get_length(i))
-    
-        num_batches = 0
-        current_token = 0
-    
-        for idx in indices:
-            length = self.dataset.get_length(idx)
-            if length + current_token > self.max_batch_tokens_cnt and current_token > 0:
-                num_batches += 1
-                current_token = 0
-            current_token += length
-    
-        if current_token > 0:
-            num_batches += 1
-    
-        return num_batches
+        return len(self.batches)
+
 
 class PaddingCollator:
-    def __init__(self,
-            token_pad_ids = 0,
-            label_pad_ids = -100,):
-        self.token_pad_ids = token_pad_ids
-        self.label_pad_ids = label_pad_ids
+    def __init__(self,token_pad = 0, label_pad = -100,):
+
+        self.token_pad = token_pad
+        self.label_pad = label_pad
 
     def __call__(self, batch):
         batch_size = len(batch)
-        max_len = max(len(item["input_ids"]) for item in batch)
-
+        max_len = max(len(x["input_ids"]) for x in batch)
         input_ids = torch.full(
-            (batch_size, max_len),
-            self.token_pad_ids,
+            (batch_size,max_len),
+            self.token_pad,
             dtype = torch.int64,
         )
 
         label_ids = torch.full(
-            (batch_size, max_len),
-            self.label_pad_ids,
-            dtype = torch.int64,
+             (batch_size,max_len),
+             self.label_pad,
+             dtype = torch.int64,
         )
 
         pad_mask = torch.zeros(
@@ -177,25 +132,14 @@ class PaddingCollator:
         )
 
         for i, item in enumerate(batch):
-            length = len(item["input_ids"])
-            input_ids[i, :length] = item["input_ids"]
-            label_ids[i, :length] = item["labels"]
-            pad_mask[i, : length] = 1
-
+             length = len(item["input_ids"])
+             input_ids[i][:length] = batch[i]["input_ids"]
+             label_ids[i][:length] = batch[i]["labels"]
+             pad_mask[i][:length] = 1
 
         return {
             "input_ids": input_ids,
             "labels": label_ids,
             "pad_mask": pad_mask,
         }
-
-#dataset = VariableLengthDataset("novel.txt")
-#print(len(dataset))
-
-
-
-
-
-
-
-
+    

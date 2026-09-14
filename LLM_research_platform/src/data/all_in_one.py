0dfe33ce6cd6c11aeac6a8739e4f1eea9db0b78c
gitchaofang <fangchao118@gmail.com>
2026-09-14 12:31:52 -0700

    update

--- LLM_research_platform/src/data/all_in_one.py
+++ LLM_research_platform/src/data/all_in_one.py
@@ -7,8 +7,8 @@ from torch.utils.data import Dataset
 from torch.utils.data import Sampler
 from src.data.regex_tokenizer import RegexTokenizer
 
-LOCAL_PATH =  Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/")
-COLAB_PATH = Path("/content/AI/LLM_research_platform/src/data/input_data/data/")
+LOCAL_PATH =  Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/index_files")
+COLAB_PATH = Path("/content/AI/LLM_research_platform/src/data/input_data/data/index_files")
 #seperation pattern
 GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""

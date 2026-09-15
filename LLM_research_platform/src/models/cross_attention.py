3d74ef0f184d475c08024b03dfd741169f4a3b2a
gitchaofang <fangchao118@gmail.com>
2026-09-14 22:24:06 -0700

    update

--- LLM_research_platform/src/models/cross_attention.py
+++ LLM_research_platform/src/models/cross_attention.py
@@ -1,9 +1,10 @@
 import torch
 from torch import nn
+import math
 
 class CrossAttention(nn.Module):
     def __init__(self,d_kv: int, d_q: int, d_model: int, n_head: int, max_len: int, pad_token = 0, dropout = 0.2): # For cross_attention k and v have the same dimension
-        super.__init__()
+        super().__init__()
         assert d_model % n_head == 0
         self.d_model = d_model
         self.d_kv = d_kv
@@ -23,14 +24,30 @@ class CrossAttention(nn.Module):
         self.attn_dropout = nn.Dropout(dropout)
         self.out_dropout = nn.Dropout(dropout)
 
-    def _dot_product(self,k_x,v_x,q_x, combined_mask):
-        B_kv, T_kv, D_kv = k_x.shape
-        B_q, T_q, D_q = q_x.shape
+    def _dot_product(self, k_x, v_x, q_x, combined_mask):
+        B, T_kv, _ = k_x.shape
+        _, T_q, _ = q_x.shape
 
         # multi_heads
-        k_v = k_v.view(B_kv, T_kv, self.n_head, self.head_dim)
-        v_v = v_v.view(B_kv, T_kv, self.n_head, self.head_dim)
-        q_v = q_v.view(B_q, T_q, self.n_head, self.head_dim)
+        k_x = k_x.view(B, T_kv, self.n_head, self.head_dim).transpose(1,2)
+        v_x = v_x.view(B, T_kv, self.n_head, self.head_dim).transpose(1,2)
+        q_x = q_x.view(B, T_q, self.n_head, self.head_dim).transpose(1,2)
+
+        # scores
+        scores = q_x @ k_x.transpose(-1,-2) #[B_q,H, T_q, D_h] @ [B_q, H, D_h, T_kv] -> [B_q, H, T_q, T_k]
+        scores = scores / math.sqrt(self.head_dim)
+        scores = scores.mask_fill(combined_mask == 0,-1e10)
+
+        attention = torch.softmax(scores, dim = -1)
+        attention = self.attn_dropout(attention)
+
+        out = attention @ v_x
+        out = out.transpose(1,2).contiguous().view(B, T_q, self.d_model)
+        out = self.out_proj(out)
+
+        # dropout on out
+        out = self.out_dropout(out)
+        return out
 
     # here masks are pad_masks. causal mask is not needed in cross-attention
     def forward(self, k_x, v_x, q_x, kv_mask,q_mask):
@@ -49,7 +66,9 @@ class CrossAttention(nn.Module):
         
         # build a combined mask
         pad_mask = q_mask.unsqueeze(-1) @ kv_mask.unsqueeze(-2) #[B_q, T_q] @ [B_k,T_k] -> [B_q, T_q, T_k]
-        combined_mask = pad_mask.unsqueez(1) # [B_q, T_q, T_k] -> [B_q, H, T_q, T_k]
+        combined_mask = pad_mask.unsqueeze(1) # [B_q, T_q, T_k] -> [B_q, 1, T_q, T_k]
+
+        return self._dot_product(k_x, v_x, q_x, combined_mask)
 
 
 

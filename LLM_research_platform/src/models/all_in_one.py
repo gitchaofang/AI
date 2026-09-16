d5422bdc3267d9b834fec25e370c1d35e93bd1a5
gitchaofang <fangchao118@gmail.com>
2026-09-16 00:15:36 -0700

    pdate

--- LLM_research_platform/src/models/all_in_one.py
+++ LLM_research_platform/src/models/all_in_one.py
@@ -2,6 +2,7 @@ import random
 import torch
 import torch.nn as nn
 import math
+import numpy as np
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from src.data.regex_tokenizer import RegexTokenizer
@@ -17,9 +18,10 @@ MAX_LEN = 256
 HIDDEN_DIM = 384
 BATCH_SIZE = 8
 ACCUMULATION_STEPS = 8
+VOCAB_SIZE = 50000
 # self attention
 class SelfAttention(nn.Module):
-    def __init__(self, d_model: int, n_heads: int, max_seq_len: int, pad_token=0, dropout = 0.2):
+    def __init__(self, d_model: int, n_heads: int, max_seq_len: int, rope_dims, RoPE=True, pad_token=0, dropout = 0.2):
         super().__init__()
         assert d_model % n_heads == 0
 
@@ -27,6 +29,14 @@ class SelfAttention(nn.Module):
         self.n_heads = n_heads
         self.max_seq_len = max_seq_len
         self.head_dim = d_model // n_heads
+
+        # M-rope config
+        self.rope_dims = rope_dims
+        self.RoPE = RoPE
+
+        assert len(rope_dims) >= 1
+        assert sum(rope_dims) == self.head_dim
+        assert all(dim % 2 == 0 for dim in rope_dims)
 
         self.q_proj = nn.Linear(d_model, d_model)
         self.k_proj = nn.Linear(d_model, d_model)
@@ -45,19 +55,103 @@ class SelfAttention(nn.Module):
 
         self.kv_cache = None
         self.kv_mask = None
+        self.kv_positions = None
+
         self.pad_token = pad_token
 
     def reset_cache(self):
         self.kv_cache = None
         self.kv_mask = None
+        self.kv_positions = None
 
-    def _dot_product(self, q, k, v, combined_mask):
+    def _apply_rope(self, x, positions):
+        '''
+        x: 
+            [B, H, T ,D]
+        
+        positions:
+            [B, T, M]
+        
+        rope_dims:
+            [M]
+            sum must equal D
+            Each dimention must be even
+        '''
+
+        B, H, T, D = x.shape
+        B_pos, T_pos, M = positions.shape
+
+        assert B_pos == B
+        assert T_pos == T
+        assert len(self.rope_dims) == M
+        assert sum(self.rope_dims) == D
+        assert all(dim % 2 == 0 for dim in self.rope_dims)
+
+        out = torch.empty_like(x)
+
+        dim_start = 0
+
+        for m, rope_dim in enumerate(self.rope_dims):
+
+            dim_end = dim_start + rope_dim
+
+            # Features assigned to positional dimension m
+            x_m = x[...,dim_start:dim_end]
+
+            # Position coordinate for dimension m
+            # [B, T]
+            position_m = positions[...,m]
+
+            # Number of rotation pairs
+            n_pairs = rope_dim // 2
+
+            # [n_pairs]
+            inv_freq = 1.0 / (
+                10000.0 ** (
+                    2.0
+                    * torch.arange(
+                        n_pairs,
+                        device=x.device,
+                        dtype=torch.float32,
+                    )
+                    / rope_dim
+                )
+            )
+
+            # [B, T, n_pairs]
+            angles = (position_m.float().unsqueeze(-1) * inv_freq)
+
+
+            # [B, 1, T, n_pairs]
+            cos = torch.cos(angles).unsqueeze(1)
+            sin = torch.sin(angles).unsqueeze(1)
+
+            # [B, H, T, n_pairs]
+            x_even = x_m[...,0::2]
+            x_odd = x_m[...,1::2]
+
+            # apply rotation
+            out[..., dim_start:dim_end:2] = (x_even * cos - x_odd * sin)
+            out[..., dim_start + 1:dim_end:2] = (x_even * sin + x_odd * cos)
+
+            dim_start = dim_end
+
+        return out
+
+    
+    def _dot_product(self, q, k, v, combined_mask, q_position, k_position):
         B, T_q, D = q.shape
         T_k = k.shape[1]
 
         q = q.view(B, T_q, self.n_heads, self.head_dim).transpose(1, 2)
         k = k.view(B, T_k, self.n_heads, self.head_dim).transpose(1, 2)
         v = v.view(B, T_k, self.n_heads, self.head_dim).transpose(1, 2)
+
+        # M-RoPE
+        if self.RoPE:
+            q = self._apply_rope(q, q_position)
+            k = self._apply_rope(k, k_position)
+        
 
         scores = q @ k.transpose(-1, -2)
         scores = scores / math.sqrt(self.head_dim)
@@ -76,7 +170,7 @@ class SelfAttention(nn.Module):
         return out
 
 
-    def forward(self, x, pad_mask, is_prefill=False, is_generate=False):
+    def forward(self, x, pad_mask, positions, is_prefill=False, is_generate=False):# For normal self-attention, k_positions can default to q_positions 
         B, T_q, D = x.shape
         pad_mask = pad_mask.to(device=x.device, dtype=torch.float32)
 
@@ -84,18 +178,30 @@ class SelfAttention(nn.Module):
         k = self.k_proj(x)
         v = self.v_proj(x)
 
+        # Current Q and current/new K
+        # have the same positions.
+        q_positions = positions
+        k_positions = positions
+
+        # ---------------------------------
+        # Generation with KV cache
+        # ---------------------------------
         if is_generate:
             if self.kv_cache is None:
                 self.kv_cache = (k, v)
                 self.kv_mask = pad_mask
+                self.kv_positions = k_positions
             else:
                 k_full = torch.cat([self.kv_cache[0], k], dim=1)
                 v_full = torch.cat([self.kv_cache[1], v], dim=1)
                 self.kv_cache = (k_full, v_full)
                 self.kv_mask = torch.cat([self.kv_mask, pad_mask], dim=1)
+                self.kv_positions = torch.cat([self.kv_positions, k_positions], dim=1,)
+
 
             k_full, v_full = self.kv_cache
             T_k = k_full.size(1)
+            assert T_k <= self.max_seq_len, ( "KV cache exceeds max_seq_len")
 
             valid_mask = self.kv_mask.unsqueeze(-1) @ self.kv_mask.unsqueeze(-2)
             valid_mask = valid_mask.unsqueeze(1)
@@ -104,17 +210,24 @@ class SelfAttention(nn.Module):
             combined_mask = causal_mask * valid_mask
             combined_mask = combined_mask[:, :, -T_q:, :]
 
-            return self._dot_product(q, k_full, v_full, combined_mask)
+            return self._dot_product(q, k_full, v_full, combined_mask, q_positions, self.kv_positions)
 
+        # ---------------------------------
+        # Prefill
+        # ---------------------------------
         if is_prefill:
             self.kv_cache = (k, v)
             self.kv_mask = pad_mask
+            self.kv_positions = k_positions
 
+        # ---------------------------------
+        # Normal training / prefill attention
+        # ---------------------------------
         pad_mask = pad_mask.unsqueeze(-1)
         pad_mask = pad_mask @ pad_mask.transpose(-1, -2)
         combined_mask = self.causal_mask[:, :, :T_q, :T_q].to(x.device).float() * pad_mask.unsqueeze(1)
 
-        return self._dot_product(q, k, v, combined_mask)
+        return self._dot_product(q, k, v, combined_mask, q_positions, k_positions)
         
 
 # transformer:
@@ -132,41 +245,50 @@ class FeedForward(nn.Module):
         return self.net(x)
 
 class TransformerBlock(nn.Module):
-    def __init__(self, d_model, n_heads, max_seq_len):
+    def __init__(self, d_model, n_heads, max_seq_len, rope_dims, RoPE=True):
         super().__init__()
         self.norm1 = nn.LayerNorm(d_model)
-        self.attention = SelfAttention(d_model, n_heads, max_seq_len)
+        self.attention = SelfAttention(d_model, n_heads, max_seq_len, rope_dims, RoPE=RoPE)
         self.norm2 = nn.LayerNorm(d_model)
         self.ffn = FeedForward(d_model)
 
-    def forward(self, x, pad_mask, is_prefill=False, is_generate=False):
-        x = x + self.attention(self.norm1(x), pad_mask, is_prefill, is_generate)
+    def forward(self, x, pad_mask, positions, is_prefill=False, is_generate=False):
+        x = x + self.attention(self.norm1(x), pad_mask, positions ,is_prefill=is_prefill, is_generate=is_generate)
         x = x + self.ffn(self.norm2(x))
         return x
 
 class GPT(nn.Module):
     def __init__(
         self,
-        vocab_size: int,
-        d_model: int,
-        n_layers: int,
-        n_heads: int,
-        max_seq_len: int,
+        vocab_size=VOCAB_SIZE,
+        d_model=HIDDEN_DIM,
+        max_seq_len=MAX_LEN,
+        n_layers=6,
+        n_heads=6,
+        rope_dims=[64],
+        RoPE=True,
     ):
         super().__init__()
 
         self.vocab_size = vocab_size
         self.d_model = d_model
         self.max_seq_len = max_seq_len
+        self.RoPE = RoPE
+        self.rope_dims = rope_dims
 
         self.token_embedding = nn.Embedding(vocab_size + 1 ,d_model,)
-        self.position_embedding = nn.Embedding(max_seq_len,d_model,)
+        # if not using RoPE, we use learnable positional embedding
+        if not RoPE:
+            self.position_embedding = nn.Embedding(max_seq_len,d_model,)
+
         self.blocks = nn.ModuleList(
             [
                 TransformerBlock(
                     d_model=d_model,
                     n_heads=n_heads,
                     max_seq_len=max_seq_len,
+                    rope_dims=rope_dims,
+                    RoPE = RoPE,
                 )
                 for _ in range(n_layers)
             ]
@@ -200,6 +322,7 @@ class GPT(nn.Module):
         self,
         x,
         pad_mask,
+        positions,
         is_prefill=False,
         is_generate=False,
     ):
@@ -207,37 +330,72 @@ class GPT(nn.Module):
 
         assert T_q <= self.max_seq_len
 
+        assert positions.ndim == 3, (
+            "positions must have shape [B, T, M]"
+        )
+
+        assert positions.shape[0] == B
+        assert positions.shape[1] == T_q
+        if self.RoPE:
+            assert positions.shape[2] == len(self.rope_dims)
+
         assert not (
             is_prefill and is_generate
         ), "is_prefill and is_generate cannot both be True"
 
-        if not is_prefill and not is_generate:
-            position_ids = torch.arange(T_q,device=x.device,).unsqueeze(0).expand(B,T_q,)
-        elif is_prefill:
-            position_ids = torch.arange(T_q,device=x.device,).unsqueeze(0).expand(B,T_q,)
-            self.position_offset = (pad_mask.to(torch.int64).sum(dim=1))
-        else:
-            assert is_generate
-            assert self.position_offset is not None, (
-                "Generation requires a previous prefill"
-            )
-            relative_positions = torch.arange(T_q,device=x.device,).unsqueeze(0)
-            position_ids = (self.position_offset.unsqueeze(1)+ relative_positions)
+        # -----------------------------------------
+        # Learned positional embeddings
+        # Only used when RoPE=False
+        # -----------------------------------------
 
-        assert torch.all(position_ids >= 0)
-        assert torch.all(position_ids < self.max_seq_len), ("Position ID exceeds max_seq_len")
+        if not self.RoPE:
 
-        x = (self.token_embedding(x)+ self.position_embedding(position_ids))
+            if not is_prefill and not is_generate:
+                position_ids = torch.arange(T_q,device=x.device,).unsqueeze(0).expand(B,T_q,)
+            elif is_prefill:
+                position_ids = torch.arange(T_q,device=x.device,).unsqueeze(0).expand(B,T_q,)
+                self.position_offset = (pad_mask.to(torch.int64).sum(dim=1))
+            else:
+                assert is_generate
+                assert self.position_offset is not None, (
+                    "Generation requires a previous prefill"
+                )
+                relative_positions = torch.arange(T_q,device=x.device,).unsqueeze(0)
+                position_ids = (self.position_offset.unsqueeze(1)+ relative_positions)
+
+            assert torch.all(position_ids >= 0)
+            assert torch.all(position_ids < self.max_seq_len), ("Position ID exceeds max_seq_len")
+
+        # -----------------------------------------
+        # Token embedding
+        # -----------------------------------------
+        x = self.token_embedding(x)
+
+        # -----------------------------------------
+        # Optional learnable positional embedding
+        # -----------------------------------------
+
+        if not self.RoPE:
+            x = (x + self.position_embedding(position_ids))
+
+        # -----------------------------------------
+        # Transformer blocks
+        # ----------------------------------------- 
         for block in self.blocks:
             x = block(
                 x,
                 pad_mask,
+                positions,
                 is_prefill=is_prefill,
                 is_generate=is_generate,
             )
         x = self.norm(x)
         logits = self.lm_linear(x)
-        if is_generate:
+
+        # -----------------------------------------
+        # Update generation position
+        # -----------------------------------------
+        if is_generate and not self.RoPE:
             self.position_offset += (pad_mask.to(torch.int64).sum(dim=1))
 
         return logits
@@ -249,9 +407,9 @@ class Trainer:
         self.optimizer = optimizer
         self.device = device
 
-    def train_step(self, x, y, mask):
+    def train_step(self, x, y, mask, positions):
 
-        logits = self.model(x, mask, is_prefill=False, is_generate=False)
+        logits = self.model(x, mask, positions, is_prefill=False, is_generate=False)
 
         B, T, V = logits.shape
         loss = F.cross_entropy(
@@ -327,6 +485,8 @@ model = GPT(
     max_seq_len = MAX_LEN,
     n_layers = 6,
     n_heads = 6,
+    rope_dims=[64],
+    RoPE=True,
 ).to(device)
 
 optimizer = torch.optim.AdamW(
@@ -354,11 +514,12 @@ for epoch in range(epoches):
         x = batch["input_ids"].to(device)
         y = batch["labels"].to(device)
         pad_mask = batch["pad_mask"].to(device)
+        positions = batch["positions"].to(device)
 
         num_tokens = pad_mask.sum().item()
         epoch_tokens += num_tokens
 
-        loss = trainer.train_step(x, y, pad_mask) / accumulation_steps
+        loss = trainer.train_step(x, y, pad_mask, positions) / accumulation_steps
         loss.backward()
 
         loss_accu += (loss.item() * accumulation_steps)
@@ -411,11 +572,12 @@ for epoch in range(epoches):
             x = batch["input_ids"].to(device)
             y = batch["labels"].to(device)
             pad_mask = batch["pad_mask"].to(device)
+            positions = batch["positions"].to(device)
 
             num_tokens = pad_mask.sum().item()
             eval_tokens += num_tokens
 
-            eval_loss_accu += trainer.train_step(x, y, pad_mask).item()
+            eval_loss_accu += trainer.train_step(x, y, pad_mask,positions,).item()
             eval_cnt += 1
     avg_eval_loss = eval_loss_accu / eval_cnt
     print(f"epoch | {epoch} | eval error: {avg_eval_loss}")
@@ -430,8 +592,3 @@ for epoch in range(epoches):
         })
 
 wandb.finish()
-
-
-
-
-    

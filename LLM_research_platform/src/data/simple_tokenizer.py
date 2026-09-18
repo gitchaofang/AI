fd83d3f2377e0fbefe9949658b47c2fe84726f90
gitchaofang <fangchao118@gmail.com>
2026-09-18 15:04:33 -0700

    update

--- LLM_research_platform/src/data/simple_tokenizer.py
+++ LLM_research_platform/src/data/simple_tokenizer.py
@@ -1,25 +1,66 @@
+from pathlib import Path
+import yaml
+import regex as re
+
+# the main GPT text split patterns, see
+# https://github.com/openai/tiktoken/blob/main/tiktoken_ext/openai_public.py
+GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
+
+FILE_DIR = Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/token_training/")
+#FILE_DIR = Path("/content/AI/LLM_research_platform/src/data/input_data/data/token_training/")
+
+
+# for tokenization:
+# 0: for padding
+# 1: for EOS
+# tokenization starts from 2
+PAD_ID = 0
+EOS_ID = 1 
+TOKEN_OFFSET = 2
 class SimpleTokenizer:
-	def __init__(self,file_name):
-		#path = "/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/" + file_name # for local
-		path = "/content/AI/LLM_research_platform/src/data/input_data/data/" + file_name # for codlab online # for codelab online
-		with open(path, "r", encoding="utf-8") as f:
-			text = f.read()
-		chars = sorted(list(set(text)))
-	#	print(f"chars: {chars} size is {len(chars)}")
+	def __init__(self, file_dir = FILE_DIR, pattern = None):
+		self.file_dir = Path(file_dir)
+		# pattern
+		self.pattern = GPT2_SPLIT_PATTERN if pattern is None else pattern
+		self.compiled_pattern = re.compile(self.pattern)
+		self.stoi = {}
+		self.itos = {}
 		
-		
+	def train(self):
+		chars = set()
+		for file_path in self.file_dir.glob("*.txt"):
+			print(f"training on {file_path}")
+			with open(file_path, "r", encoding = "utf-8") as f:
+				text = f.read()
+				chars.update(text)
+
+		chars = sorted(list(set(self.text)))
 		self.stoi = {
-			char: i + 1 
-			for i, char in enumerate(chars)
-		}
+				char: i + TOKEN_OFFSET 
+				for i, char in enumerate(chars)
+			}
 		self.itos = {
-			i + 1: char
-			for i, char in enumerate(chars)
-		}
+				i + TOKEN_OFFSET: char
+				for i, char in enumerate(chars)
+			}
+	def _encode_chunk(self, chunk_text):
+		ids = [self.stoi[char] for char in chunk_text]
+		return ids
 	
-	def get(self):
-		return {
-			"stoi": self.stoi,
-			"itos": self.itos
-		}
-		
+	def encode(self, text):
+		chunks_text = self.compiled_pattern.findall(text)
+		encoded_ids = []
+
+		for chunk_text in chunks_text:
+			encoded_ids.append(self._encode_chunk(chunk_text))
+
+		# deal with EOS
+		if encoded_ids:
+			encoded_ids[-1].append(EOS_ID)
+		else:
+			encoded_ids.append([EOS_ID])
+		return encoded_ids
+	
+	def decode(self, ids):
+		return "".join(self.itos[id] for id in ids)		

f291094ff19ba58c0c52af38aa0172aeadcb6354
gitchaofang <fangchao118@gmail.com>
2026-09-18 14:05:32 -0700

    update

--- LLM_research_platform/src/data/regex_tokenizer.py
+++ LLM_research_platform/src/data/regex_tokenizer.py
@@ -16,7 +16,7 @@ FILE_DIR = Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_resea
 LOCAL_YAML_PATH = Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/configs/gpt.yaml")
 COLAB_YAML_PATH = Path("/content/AI/LLM_research_platform/configs/gpt.yaml")
 # load yaml config
-with open(LOCAL_YAML_PATH,"r") as f:
+with open(COLAB_YAML_PATH,"r") as f:
     config = yaml.safe_load(f)
 
 

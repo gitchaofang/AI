e10959c57cf1804916e25447d92327bbea1e954b
gitchaofang <fangchao118@gmail.com>
2026-09-22 20:51:15 -0700

    update

--- LLM_research_platform/src/data/dataset.py
+++ LLM_research_platform/src/data/dataset.py
@@ -1,15 +1,26 @@
 import torch
 import os
+import json
+import yaml
+from PIL import Image
+from torchvision import transforms
 import numpy as np
 from pathlib import Path
 
 from torch.utils.data import Dataset
 from src.data.simple_tokenizer import SimpleTokenizer
 
-LOCAL_FILE_PATH =  Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/")
-LOCAL_INDEX_PATH =  Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/src/data/input_data/data/index_files")
-COLAB_FILE_PATH = Path("/content/AI/LLM_research_platform/src/data/input_data/data/")
-COLAB_INDEX_PATH = Path("/content/AI/LLM_research_platform/src/data/input_data/data/index_files")
+# load config file
+LOCAL_YAML_PATH = Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/configs/gpt.yaml")
+COLAB_YAML_PATH = Path("/content/AI/LLM_research_platform/configs/gpt.yaml")
+# load yaml config
+with open(COLAB_YAML_PATH,"r") as f:
+    gpt_config = yaml.safe_load(f)
+
+LOCAL_FILE_PATH =  Path(gpt_config["data"]["local_file_path"])
+LOCAL_INDEX_PATH =  Path(gpt_config["data"]["local_index_path"])
+COLAB_FILE_PATH = Path(gpt_config["data"]["colab_file_path"])
+COLAB_INDEX_PATH = Path(gpt_config["data"]["colab_index_path"])
 
 # text
 class TextDecodeDataset(Dataset): # for txt file
@@ -79,7 +90,70 @@ class TextDecodeDataset(Dataset): # for txt file
     def get_length(self, key):
          return self.index[key][1]
 
-# image
-# I use google drrive as disk for store data. The path is:
+# -------------------------------------------------
+# image dala loading
+# I use google drive as disk for store data. The path is:
+# extracted data: /content/drive/MyDrive/VLM_DATA/CC3M/normal 
+# shareds data: /content/drive/MyDrive/VLM_DATA/CC3M/shards
 # index_map{index, image-data name}
 # use inex_map to fetch data from google drive during training.
+
+# load config file
+LOCAL_YAML_PATH = Path("/Users/chaofang/Documents/coding_playground/GitHub/AI/LLM_research_platform/configs/vit.yaml")
+COLAB_YAML_PATH = Path("/content/AI/LLM_research_platform/configs/vit.yaml")
+# load yaml config
+with open(COLAB_YAML_PATH,"r") as f:
+    vit_config = yaml.safe_load(f)
+
+EXTRACTED_PATH  = Path(vit_config["data"]["extracted_path"]) 
+SHARED_PATH = Path(vit_config["data"]["shared_path"])
+
+class ImageTextEncode(Dataset):
+    def __init__(self, file_dir = EXTRACTED_PATH):
+        self.stem_dict = []
+        self.file_dir = Path(file_dir)
+        self.file_cnt = 0
+        self.dict_path = EXTRACTED_PATH/"index.json"
+        if not (os.path.exists(self.dict_path)):
+            self.file_cnt = 0
+            self._build_index()
+        else:
+            with open(self.dict_path, "r") as f:
+                self.stem_dict = json.load(f)
+         
+    def _build_index(self):
+        for file_path in self.file_dir.glob("*.txt"):
+            stem = file_path.stem
+            self.stem_dict.appends(stem)
+            self.file_cnt += 1
+        with open(self.dict_path,"w") as f:
+            json.dump(self.stem_dict, f)  
+
+    def __len__(self):
+        return self.file_cnt
+
+    def __getitem__(self, key):
+        assert key < self.file_cnt, "key is out of range of index"
+        stem = self.index_dict[key]
+        image_path = self.file_dir / f"{stem}.jpg"
+        meta_data_path = self.file_dir / f"{stem}.json"
+
+        # fetch image and text
+        image = Image.open(image_path).convert("RBG")
+        to_tensor = transforms.ToTensor()
+        x = to_tensor(image)
+        # json keys:
+        # "caption", "url", "key", "status", "width", "height", "exif", "original_width", "original_heigth"
+        with open(meta_data_path, "r") as f:
+            meta_data = json.load(f)
+
+        return {
+            "image": x,
+            "meta_data": meta_data 
+        }
+        
+
+        
+
+
+

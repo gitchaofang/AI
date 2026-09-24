5e8948b37f47cd287251d16dfdd61c504e233325
gitchaofang <fangchao118@gmail.com>
2026-09-23 19:30:30 -0700

    update

--- LLM_research_platform/src/data/image_dataset_sampler.py
+++ LLM_research_platform/src/data/image_dataset_sampler.py
@@ -5,22 +5,18 @@ from torch.utils.data import BatchSampler
 class ImageDatasetBatchSampler(BatchSampler):
 
     def __init__(self, dataset, batch_size, shuffle=True):
-
         self.dataset = dataset
         self.batch_size = batch_size
         self.shuffle = shuffle
 
     def __iter__(self):
-
         indices = list(range(len(self.dataset)))
 
         if self.shuffle:
             random.shuffle(indices)
 
         for i in range(0, len(indices), self.batch_size):
-
             batch = indices[i:i + self.batch_size]
-
             yield batch
 
     def __len__(self):

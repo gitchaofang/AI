d046b81d1d286218dc71e25b07448db9e671c7f4
gitchaofang <fangchao118@gmail.com>
2026-09-23 18:00:01 -0700

    update

--- LLM_research_platform/src/data/helper.py
+++ LLM_research_platform/src/data/helper.py
@@ -2,6 +2,7 @@ import regex as re
 import torch
 import numpy as np
 from PIL import Image
+import totch.nn.functional as F
 
 
 # text tokenization
@@ -32,6 +33,76 @@ def imageToTensor(image):
 
     return tensor
 
+def pad_to_patch_grid(image, patch_size = 16, pad_value = [0.0,0.0,0.0,]): 
+    '''
+    image: [C,H,W]
+    pad_value: [C]
+    '''
+    C, H ,W = image.shape
+    H_pad = ((H + patch_size - 1) // patch_size) * patch_size
+    W_pad = ((H + patch_size - 1) // patch_size) * patch_size 
 
-        
+    padded = image.new_empty(C,H_pad, W_pad)
+
+    # Fill with channel-specific values
+    for c in range(C):
+        padded[C].fill_(pad_value[c])
+
+    #Copy original image
+    padded[:,:H,:W] = image
+    return padded
+
+def patchify(image, patch_size = 16, pad_value = [0.0, 0.0, 0.0]):
+    '''
+    image: [C,H,W]
+    returns:
+        patches: [N, C * patch_size * patch_size]
+    '''
+    # make images ready for patchify
+    pad_image = pad_to_patch_grid(image, patch_size, pad_value = pad_value)
+
+    C,H,W = image.shape
+    assert H % patch_size == 0
+    assert W % patch_size == 0
+
+    # [C,W,H]
+
+    patches = pad_image.unfold(dimension=1, size=patch_size, step=patch_size)
+    patches = patches.unfold(dimension=2, size=patch_size, step=patch_size)
+
+    # [C, H_patches, W_patches, P, P]
+    patches = patches.permute(1,2,0,3,4)
+
+    H_patches = H // patch_size
+    W_patches = W // patch_size
+
+    # [N, C * P * P]
+    patches = patches.reshape(
+        H_patches * W_patches,
+        C * patch_size * patch_size,
+    )
+
+    return patches
+
+def make_2d_positions(H_patches, W_patches):
+    """
+    Returns:
+        positions: [N, 2]
+
+    positions[:, 0] = x / column
+    positions[:, 1] = y / row
+    """
+
+    y, x = torch.meshgrid(
+        torch.arange(H_patches),
+        torch.arange(W_patches),
+        indexing="ij",
+    )
+
+    positions = torch.stack(
+        [x.flatten(), y.flatten()],
+        dim=-1,
+    )
+
+    return positions
 

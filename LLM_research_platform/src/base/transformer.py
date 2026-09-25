fb4cf22ebbb1c796dd7b759dc6ec6e1e6677fcad
gitchaofang <fangchao118@gmail.com>
2026-09-24 21:26:07 -0700

    update

--- LLM_research_platform/src/base/transformer.py
+++ LLM_research_platform/src/base/transformer.py
@@ -35,34 +35,70 @@ class FeedForward(nn.Module):
         return self.net(x)
 
 class TransformerBlock(nn.Module):
-    def __init__(self, d_model, n_heads, max_seq_len, rope_dims, RoPE=gpt_config["model"]["rope"], cross_attention = vit_config["model"]["cross_attention"], ):
+    def __init__(self, 
+                 d_model, 
+                 n_heads, 
+                 max_seq_len, 
+                 rope_dims, 
+                 RoPE=True, 
+                 cross_attention_enabled=True,
+                 dropout = 0.2 
+    ):
         super().__init__()
+        self.cross_attention_enabled = cross_attention_enabled
         self.norm1 = nn.LayerNorm(d_model)
-        self.attention = SelfAttention(d_model, n_heads, max_seq_len, rope_dims, RoPE=RoPE)
-        if cross_attention:
+        self.attention = SelfAttention(d_model=d_model, 
+                                       n_hdaes=n_heads, 
+                                       max_seq_len=max_seq_len, 
+                                       rope_dims=rope_dims, 
+                                       RoPE=RoPE)
+        self.norm2 = nn.LayerNorm(d_model)
+
+        if cross_attention_enabled:
             # parameters for content(vision) side
             self.cross_attention = CrossAttention(
                 d_kv=vit_config["model"]["d_model"],
                 d_q=d_model,
                 n_head=n_heads,
                 dropout=gpt_config["model"]["dropout"])
-            self.norm3 = nn.LayerNorm(d_model)     
-        self.norm2 = nn.LayerNorm(d_model)
-        self.ffn = FeedForward(d_model)
+        self.norm3 = nn.LayerNorm(d_model)     
+        self.ffn = FeedForward(d_model=d_model,
+                               mlp_ratio = gpt_config["model"]["mlp_ratio"],
+                               dropout = dropout)
 
-    def forward(self, x, pad_mask, positions, is_prefill=False, is_generate=False, content = None,): # content_input is a dict: {"patches", "patch_mask"}
-        '''
+    def forward(self, x, pad_mask, positions, is_prefill=False, is_generate=False, content = None,):
+        """
+        Text:
+            x: [B, T, D]
+        Image content:
+            content["patches"]: [B, N, D]
+            content["mask"]:    [B, N]
+
+        Output:
+            [B, T, D]
+        """ 
+        """
         Text -> LayerNorm -> Self-Attention -> Residual -> LayerNorm -> Cross-Attention -> Residual -> LayerNorm -> FFN -> Residual
-        '''
-        assert (content is None and not self.cross_attention) or (content and self.self.cross_attention)
-        # self_attention
-        x = x + self.attention(self.norm1(x), pad_mask, positions ,is_prefill=is_prefill, is_generate=is_generate)
-        # cross_attention
-        if self.cross_attention:
-            x = x + self.cross_attention(content = content["patches"], 
-                                        q_x = self.norm2(x), 
-                                        kv_mask = content["mask"], 
-                                        q_mask = pad_mask)
+        """
+        # 1. Causal text self-attention
+        x = x + self.attention(
+            x=self.norm1(x), 
+            pad_mask=pad_mask, 
+            positions=positions,
+            is_prefill=is_prefill, 
+            is_generate=is_generate
+        )
+        # 2.Text -> image cross-attention
+        if self.cross_attention_enabled:
+            x = x + self.cross_attention(
+                content = content["patches"], 
+                q_x = self.norm2(x), 
+                kv_mask = content["mask"], 
+                q_mask = pad_mask,
+            )
+        else:
+            assert content is None
+        # 3. Feed-forward network
         x = x + self.ffn(self.norm3(x))
         
         return x
